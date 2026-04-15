3289f7df90346146257ef136c9a61607f70660b1
Syadda Abdullah <ssyyaaddddaa@gmail.com>
2026-04-16 03:40:37 +0700

    Hardcode region AWS ke ap-southeast-2 (From Server)

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    region: 'ap-southeast-2' 
 });
 
 // Menggunakan memoryStorage agar file tidak membebani hardisk EC2
